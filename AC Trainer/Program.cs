4f793b784dababc9d9f1b36818d56e25642745cd
L950x <loick.lp59@gmail.com>
2024-11-02 22:47:21 +0100

    Ajoutez des fichiers projet.

---
+++ AC Trainer/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using AC_Trainer;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using Swed32;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+
+class Program
+{
+
+
+    static void Main()
+    {
+        Renderer renderer = new Renderer();
+        Thread rendererThread = new Thread(new ThreadStart(renderer.Start().Wait));
+
+        Swed swed = new Swed("ac_client");
+        IntPtr moduleBase = swed.GetModuleBase("ac_client.exe");
+        int AmmoAddressOffset = 0x190844;
+        int localPlayerOffset = 0x0017E0A8;
+        int healthOffset = 0xEC;
+        int YOffset = 0x30;
+        //int cameraYOffset = 0x38;
+        //int healthSubInstruction = 0x84499;
+        //string healthBytes = "89 82 EC 00 00 00";
+        IntPtr localPlayerAddress = swed.ReadPointer(moduleBase, localPlayerOffset);
+        IntPtr ammoAddress = swed.ReadPointer(moduleBase, AmmoAddressOffset, 0x0);
+
+
+        while (true)
+        {
+
+            /*
+            if (GetAsyncKeyState(0x01) < 0) {
+                float cameraY = swed.ReadFloat(localPlayerAddress, cameraYOffset);
+                swed.WriteFloat(localPlayerAddress, cameraYOffset, cameraY);
+                Thread.Sleep(10);
+            }
+            */
+
+            if (renderer.getAmmo)
+            {
+                setAmmo(ammoAddress, swed, 1000);
+            }
+            else
+            {
+                int ammoValue = swed.ReadInt(ammoAddress, 0x140);
+                if (ammoValue > 50)
+                {
+                    setAmmo(ammoAddress, swed, 30);
+                }
+            }
+
+            if (renderer.getHealth)
+            {
+                setHealth(localPlayerAddress, healthOffset, swed, 100);
+            }
+
+            if (renderer.fly)
+            {
+                float Y = swed.ReadFloat(localPlayerAddress, YOffset);
+                toggleFly(localPlayerAddress, swed, Y, YOffset);
+            }
+
+        }
+    }
+
+    static public void setAmmo(IntPtr ammoAddress, Swed swed, int value)
+    {
+        swed.WriteInt(ammoAddress, 0x140, value);
+    }
+
+    static public void setHealth(IntPtr localPlayerAddress, int healthOffset, Swed swed, int value)
+    {
+        swed.WriteInt(localPlayerAddress, healthOffset, value);
+    }
+
+    static DateTime lastUpdateTime = DateTime.Now;
+
+    static public void toggleFly(IntPtr localPlayerAddress, Swed swed, float Y, int YOffset)
+    {
+        if (GetAsyncKeyState(0x20) < 0)
+        {
+            if (Y <= 9)
+            {
+                float newPosition = Y + 0.0005f;
+                swed.WriteFloat(localPlayerAddress, YOffset, newPosition);
+                lastUpdateTime = DateTime.Now;
+            }
+            else
+            {
+                Y = 2;
+            }
+
+        }
+
+        if (Y > 5 && (DateTime.Now - lastUpdateTime).TotalSeconds > 1)
+        {
+            Y = 1;
+            swed.WriteFloat(localPlayerAddress, YOffset, Y);
+        }
+
+    }
+
+
+    [DllImport("user32.dll")]
+    static extern short GetAsyncKeyState(int vKey);
+
+}
---
+++ AC Trainer/Renderer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+using Vortice.Mathematics;
+
+namespace AC_Trainer
+{
+
+    internal class Renderer : Overlay
+    {
+        private bool prevKeyState = false;
+        public Vector2 screenSize = new Vector2(1536, 864);
+
+        public bool getAmmo = false;
+        public bool getHealth = false;
+        public bool rapidFire = false;
+        public bool fly = false;
+        public bool noReload = false;
+
+
+        protected override void Render()
+        {
+            ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(0.161f, 0f, 0.141f, 0.5f));
+            ImGui.PushStyleColor(ImGuiCol.Header, new Vector4(0.227f, 0f, 0.196f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.TitleBgActive, new Vector4(0.227f, 0f, 0.196f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.Text, new Vector4(1.0f, 1.0f, 1.0f, 1.0f));
+            ImGui.PushStyleColor(ImGuiCol.FrameBg, new Vector4(0.33f, 0f, 0.298f, 1f));
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 10.0f);
+            ImGui.SetNextWindowSize(new Vector2(300, 150), ImGuiCond.Always);
+            ImGui.Begin("Assault Cube Trainer v1.0", ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.AlwaysAutoResize);
+            ImGui.Checkbox("Infinite Ammo", ref getAmmo);
+            ImGui.Checkbox("God Mode", ref getHealth);
+            ImGui.Checkbox("Rapid Fire", ref rapidFire);
+            ImGui.Checkbox("Fly", ref fly);
+            ImGui.Checkbox("No Reload", ref noReload);
+            ImGui.End();
+
+            DrawOverlay(screenSize);
+
+            //prevKeyState = keyState;
+
+            ImGui.PopStyleColor(5);
+            ImGui.PopStyleVar(1);
+        }
+
+        void DrawOverlay(Vector2 screenSize)
+        {
+            ImGui.SetNextWindowSize(screenSize);
+            ImGui.SetNextWindowPos(new Vector2(0, 0));
+            ImGui.Begin("overlay", ImGuiWindowFlags.NoDecoration
+                | ImGuiWindowFlags.NoBackground
+                | ImGuiWindowFlags.NoBringToFrontOnFocus
+                | ImGuiWindowFlags.NoMove
+                | ImGuiWindowFlags.NoInputs
+                | ImGuiWindowFlags.NoCollapse
+                | ImGuiWindowFlags.NoScrollbar
+                | ImGuiWindowFlags.NoScrollWithMouse
+                );
+        }
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int vKey);
+    }
+}
